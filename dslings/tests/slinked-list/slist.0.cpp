40c3659355e77c171ec241c82eececcb7c4fd20e
SPeak <speakshen@163.com>
2024-06-30 11:49:12 +0800

    update

--- dslings/tests/slinked-list/slist.0.cpp
+++ dslings/tests/slinked-list/slist.0.cpp
@@ -4,7 +4,7 @@
 //   定义SLinkedList及其数据成员
 //
 // 目标/要求:
-//  - 定义SLinkedList及内部的链表节点Node及其数据成员
+//  - 定义SLinkedList及链表节点Node和对应的数据成员
 //  - 在exercises/linked-list/SLinkedList.hpp中完成你的代码设计
 //  - 通过所有编译器检测 和 断言
 //
@@ -15,6 +15,7 @@
 
 int main() {
 
+    d2ds::SLinkedListNode<int> node;
     d2ds::SLinkedList<int> intList;
     d2ds::SLinkedList<double, d2ds::DefaultAllocator> doubleList;
 
