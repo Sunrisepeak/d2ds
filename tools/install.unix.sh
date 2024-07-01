661f716a22015e963a34f3c14c52b4d638b3b1c9
SPeak <speakshen@163.com>
2024-07-02 04:42:32 +0800

    tools: update d2ds install script
    1. add vscode cpp-extension install
    2. add log color
    3. add tips for d2ds
    
    Signed-off-by: SPeak <speakshen@163.com>

--- tools/install.unix.sh
+++ tools/install.unix.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
 
+# ANSI color codes
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+PURPLE='\033[35m'
+CYAN='\033[36m'
+RESET='\033[0m'
+
 # check xmake status
+echo -e "${PURPLE}[d2ds]: start detect environment and try to auto config...${RESET}"
 if ! command -v xmake &> /dev/null
 then
+    echo -e "${PURPLE}[d2ds]: start install xmake...${RESET}"
     curl -fsSL https://xmake.io/shget.text | bash
 else
-    echo "xmake installed"
+    echo -e "${GREEN}[d2ds]: xmake installed${RESET}"
 fi
+
+if code --list-extensions | grep -q ms-vscode.cpptools
+then
+    echo -e "${GREEN}[d2ds]: vscode c++ extension installed${RESET}"
+else
+    echo -e "${PURPLE}[d2ds]: start install vscode c++ extension...${RESET}"
+    code --install-extension ms-vscode.cpptools
+fi
+
+echo -e "${PURPLE}[d2ds]: auto config done.${RESET}"
+
+echo -e "${YELLOW}\tDive into Data Structures pre-v0.0.1 - open-sources${RESET}\n"
+echo -e "${CYAN} - main-repo: https://github.com/Sunrisepeak/d2ds${RESET}"
+echo -e "${CYAN} - ebook: https://sunrisepeak.github.io/d2ds${RESET}"
+echo -e "${CYAN} - courses: https://sunrisepeak.github.io/d2ds-courses${RESET}"
+echo -e "${CYAN} - dstruct: https://github.com/Sunrisepeak/dstruct${RESET}"
+echo -e "${CYAN} - issues: https://github.com/Sunrisepeak/d2ds/issues${RESET}"
+
+echo -e "\n[d2ds]: Try running ${RED}xmake dslings${RESET} to start your d2ds coding..."
+echo -e "[d2ds]: run ${RED}xmake d2ds${RESET} get more info"
--- tools/install.win.bat
+++ tools/install.win.bat
@@ -1,13 +1,46 @@
 @echo off
+
+set "RED=%ESC%[31m"
+set "GREEN=%ESC%[32m"
+set "YELLOW=%ESC%[33m"
+set "PURPLE=%ESC%[35m"
+set "CYAN=%ESC%[36m"
+set "RESET=%ESC%[0m"
+
+echo %GREEN%[d2ds]: start detect environment and try to auto config...%RESET%
+
 REM Check if xmake is installed
 where xmake >nul 2>&1
 
 IF %ERRORLEVEL% EQU 0 (
-    echo xmake installed
-    GOTO END
+    echo %PURPLE%[d2ds]: xmake installed%RESET%
+    ::  GOTO END
+) else (
+    REM xmake is not installed, downloading and running install script using PowerShell
+    echo %PURPLE%[d2ds]: start install xmake...%RESET%
+    powershell -Command "Invoke-Expression ((Invoke-WebRequest 'https://xmake.io/psget.text' -UseBasicParsing).Content)"
 )
 
-REM xmake is not installed, downloading and running install script using PowerShell
-powershell -Command "Invoke-Expression ((Invoke-WebRequest 'https://xmake.io/psget.text' -UseBasicParsing).Content)"
+REM Check if the extension is already installed
+code --list-extensions | findstr /I /C:"ms-vscode.cpptools" >nul
+if %ERRORLEVEL% equ 0 (
+    echo %GREEN%[d2ds]: vscode c++ extension installed%RESET%
+) else (
+    echo %PURPLE%[d2ds]: start install vscode c++ extension...%RESET%
+    code --install-extension ms-vscode.cpptools
+)
+
+REM Colored echo
+echo %PURPLE%[d2ds]: auto config done.%RESET%
+echo %YELLOW%    Dive into Data Structures pre-v0.0.1 - open-sources%RESET%
+echo.
+echo %CYAN% - main-repo: https://github.com/Sunrisepeak/d2ds%RESET%
+echo %CYAN% - ebook: https://sunrisepeak.github.io/d2ds%RESET%
+echo %CYAN% - courses: https://sunrisepeak.github.io/d2ds-courses%RESET%
+echo %CYAN% - dstruct: https://github.com/Sunrisepeak/dstruct%RESET%
+echo %CYAN% - issues: https://github.com/Sunrisepeak/d2ds/issues%RESET%
+echo.
+echo [d2ds]: Try running %RED%xmake dslings%RESET% to start your d2ds coding...
+echo [d2ds]: run %RED%xmake d2ds%RESET% get more info
 
 :END
