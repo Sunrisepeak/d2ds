0fd164d50a2c0e6d8b8d1df9664ff5c907e72f5e
SPeak <speakshen@163.com>
2024-07-02 05:52:46 +0800

    update 1

--- tools/install.win.bat
+++ tools/install.win.bat
@@ -1,8 +1,6 @@
 @echo off
 
-REM set ansi color
-set "ESC="
-for /F "delims=" %%i in ('echo prompt $E ^& for %%E in (1) do rem') do set "ESC=%%i"
+REM set ansi color - TODO: support log-color
 set "RED=%ESC%[31m"
 set "GREEN=%ESC%[32m"
 set "YELLOW=%ESC%[33m"
@@ -10,45 +8,39 @@ set "PURPLE=%ESC%[35m"
 set "CYAN=%ESC%[36m"
 set "RESET=%ESC%[0m"
 
-REM checker ansi color support
-for /F "delims=" %%i in ('ver') do set version=%%i
-if "%version:~22,2%" GEQ "10" (
-    >nul 2>&1 reg add HKCU\Console /v VirtualTerminalLevel /t REG_DWORD /d 1 /f
-)
-
-echo %GREEN%[d2ds]: start detect environment and try to auto config...%RESET%
+echo [d2ds]: start detect environment and try to auto config...
 
 REM Check if xmake is installed
 where xmake >nul 2>&1
 
 IF %ERRORLEVEL% EQU 0 (
-    echo %PURPLE%[d2ds]: xmake installed%RESET%
+    echo [d2ds]: xmake installed
 ) else (
     REM xmake is not installed, downloading and running install script using PowerShell
-    echo %PURPLE%[d2ds]: start install xmake...%RESET%
+    echo [d2ds]: start install xmake...
     powershell -Command "Invoke-Expression ((Invoke-WebRequest 'https://xmake.io/psget.text' -UseBasicParsing).Content)"
 )
 
 REM Check if the extension is already installed
 code --list-extensions | findstr /I /C:"ms-vscode.cpptools" >nul
 if %ERRORLEVEL% equ 0 (
-    echo %GREEN%[d2ds]: vscode c++ extension installed%RESET%
+    echo [d2ds]: vscode c++ extension installed
 ) else (
-    echo %PURPLE%[d2ds]: start install vscode c++ extension...%RESET%
-    code --install-extension ms-vscode.cpptools
+    echo [d2ds]: start install vscode c++ extension...
+    call code --install-extension ms-vscode.cpptools
 )
 
 REM Colored echo
-echo %PURPLE%[d2ds]: auto config done.%RESET%
-echo %YELLOW%    Dive into Data Structures pre-v0.0.1 - open-sources%RESET%
+echo [d2ds]: auto config done.
+echo \tDive into Data Structures pre-v0.0.1 - open-sources
 echo.
-echo %CYAN% - main-repo: https://github.com/Sunrisepeak/d2ds%RESET%
-echo %CYAN% - ebook: https://sunrisepeak.github.io/d2ds%RESET%
-echo %CYAN% - courses: https://sunrisepeak.github.io/d2ds-courses%RESET%
-echo %CYAN% - dstruct: https://github.com/Sunrisepeak/dstruct%RESET%
-echo %CYAN% - issues: https://github.com/Sunrisepeak/d2ds/issues%RESET%
+echo - main-repo: https://github.com/Sunrisepeak/d2ds
+echo - ebook: https://sunrisepeak.github.io/d2ds
+echo - courses: https://sunrisepeak.github.io/d2ds-courses
+echo - dstruct: https://github.com/Sunrisepeak/dstruct
+echo - issues: https://github.com/Sunrisepeak/d2ds/issues
 echo.
-echo [d2ds]: Try running %RED%xmake dslings%RESET% to start your d2ds coding...
-echo [d2ds]: run %RED%xmake d2ds%RESET% get more info
+echo [d2ds]: Try running xmake dslings to start your d2ds coding...
+echo [d2ds]: run xmake d2ds get more info
 
 :END
